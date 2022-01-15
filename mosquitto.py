ae48f15cb5a364d4c027d5cd36a93e128e465191
maciekbaczmanski <cakeisalie16@gmail.com>
2022-01-15 22:56:23 +0100

    mqtt

--- mosquitto.py
+++ mosquitto.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-
+import time
 
 def on_message(client, userdata, message):
     global bedroom_delay, switch_off
@@ -11,10 +11,11 @@ def on_message(client, userdata, message):
 broker_address = "192.168.0.123"
 client = mqtt.Client()
 client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
-client.on_message = on_message
+# client.on_message = on_message
 client.connect(broker_address, 1881)
-client.loop_start()
-client.subscribe([("test", 0), ])
+# client.loop_start()
+# client.subscribe([("test", 0), ])
 
 while True:
-    pass
+    client.publish("test2", "ok?", qos=0, retain=False)
+    time.sleep(1)
