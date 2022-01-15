7948f986105b9cd77c67af05ed1fda7a0f826194
pudliss <jakub.pudlo@outlook.com>
2022-01-15 22:21:58 +0100

    ss

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 #GPIO SETUP
 channel = 17
-channel_pir = 2
+channel_pir = 27
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 GPIO.setup(channel_pir, GPIO.IN) 
@@ -12,22 +12,19 @@ def callback(channel):
         else:
                 print ('Sound Detected!')
 
+def callback(channel):
+        if GPIO.input(channel):
+                print ('switch')
+        else:
+                print ('switch')
+
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_detect(channel_pir, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel_pir, callback_pir)  # assign function to GPIO PIN, Run function on change
+
 # infinite loop
 j=0
 z=0
 while True:
-    i=GPIO.input(channel_pir)
-    if i==0:                 #When output from motion sensor is LOW
-        z=z+1
-        if z==2:
-            j=0
-            print ("No Intruder detected")
-        time.sleep(0.1)
-    elif i==1: 
-        z=0
-        j=j+1
-        if j > 15:
-            print ("Intruder detected")
-        time.sleep(0.1)
+    time.sleep(1)
