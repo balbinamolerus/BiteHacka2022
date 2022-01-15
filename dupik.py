570b12d3cb19a5a7574e04e5bc45f717b5ccab0c
maciekbaczmanski <cakeisalie16@gmail.com>
2022-01-15 23:19:30 +0100

    added mqtt

--- dupik.py
+++ dupik.py
@@ -1,7 +1,28 @@
 from time import sleep
+import paho.mqtt.client as mqtt
 from gpiozero import MotionSensor
+from gpiozero import Servo
+
 
 pir = MotionSensor(4)
+servo = Servo(17)
+
+def on_message(client, userdata, message):
+    global servo
+    if message.topic == "position":
+        # client.publish("test2", "ok", qos=0, retain=False)
+        pos = int(message.payload.decode("utf-8"))
+        servo.value = (pos/50)-1
+
+
+broker_address = "192.168.0.123"
+client = mqtt.Client()
+client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
+client.on_message = on_message
+client.connect(broker_address, 1881)
+client.loop_start()
+client.subscribe([("position", 0), ])
+
 
 while True:
     motion = 0
