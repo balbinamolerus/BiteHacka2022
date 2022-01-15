6081a536bc0cfcc8f2ed71d19d6f89018f28f9d7
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-01-15 17:43:43 +0100

    nagrywanko

--- dupik.py
+++ dupik.py
@@ -3,10 +3,11 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(14, GPIO.IN)
+GPIO.setup(14, GPIO.OUT)
 
 GPIO.output(14, 0)
 
+GPIO.setup(14, GPIO.IN)
 while True:
     if GPIO.input(14):
         print('ruch')
