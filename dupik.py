94d38b127d524abce627f54b79541671e9977ea7
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-01-15 20:12:13 +0100

    nagrywanko

--- dupik.py
+++ dupik.py
@@ -1,16 +1,13 @@
-
 from time import sleep
 from gpiozero import MotionSensor
+
 pir = MotionSensor(4)
 
 while True:
-  x=0
-  t=0
-  while t<50:
-    if pir.wait_for_motion()==True:
-      x=x+1
-    sleep(0.1)
-    if x>20:
-      print('alarm')
-      x=0
-    t=t+1
+    motion = 0
+    for i in range(30):
+        if pir.wait_for_motion() == True:
+            motion += 1
+        sleep(0.01)
+    if motion > 10:
+        print('alarm')
