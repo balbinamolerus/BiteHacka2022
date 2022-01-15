a14a95c76e5d56a3ef2d323332c27913669c5ebd
maciekbaczmanski <cakeisalie16@gmail.com>
2022-01-15 22:34:48 +0100

    initial telebot commit

---
+++ mosquitto.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+
+
+def on_message(client, userdata, message):
+    global bedroom_delay, switch_off
+    if message.topic == "test" and str(message.payload.decode("utf-8")) == "test1":
+        client.publish("test2", "ok", qos=0, retain=False)
+
+
+
+broker_address = "192.168.0.123"
+client = mqtt.Client()
+client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
+client.on_message = on_message
+client.connect(broker_address, 1881)
+client.loop_start()
+client.subscribe([("test", 0), ])
+
+while True:
+    pass
