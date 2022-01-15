20b49b5c8f4cb2df182efde7bfcff6704b29b15c
pudliss <jakub.pudlo@outlook.com>
2022-01-15 22:16:22 +0100

    switch

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 #GPIO SETUP
 channel = 17
-channel_pir = 27
+channel_pir = 2
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 GPIO.setup(channel_pir, GPIO.IN) 
