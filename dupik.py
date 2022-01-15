afb39cb17d6fa110b80b1a3c364c74db6fb80033
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-01-15 17:42:33 +0100

    nagrywanko

--- dupik.py
+++ dupik.py
@@ -1,8 +1,12 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD
+GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(14, GPIO.IN)
+
+GPIO.write(14, LOW)
+
 while True:
     if GPIO.input(14):
         print('ruch')
