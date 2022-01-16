f521134f10d8471dda5f54afb03f69736c4c95a2
maciekbaczmanski <cakeisalie16@gmail.com>
2022-01-16 09:42:52 +0100

    removed prints

--- budzikos.py
+++ budzikos.py
@@ -4,8 +4,8 @@ from datetime import datetime, date
 import numpy as np
 import sounddevice as sd
 import paho.mqtt.client as mqtt
-import datetime
 
+# import datetime
 
 
 alarmen = False
@@ -48,6 +48,7 @@ def warningAlarm():
         time.sleep(0.5)
     GPIO.output(alarmPin, GPIO.HIGH)
 
+
 def bigAlarm():
     for i in range(5):
         GPIO.output(alarmPin, GPIO.HIGH)
@@ -66,12 +67,14 @@ def clock():
 
 
 def audio_callback(indata, frames, time, status):
-   global client, currAlarm
-   volume_norm = np.linalg.norm(indata) * 10
-   if volume_norm > 700:
-      client.publish(f'[{(datetime.datetime.today()).strftime("%H:%M:%S")}] LoundNoise', 'Alarm', qos=0, retain=False)
-      currAlarm = 2
-      # print(f'[{(datetime.datetime.today()).strftime("%H:%M:%S")}] Published')
+    global client, currAlarm
+    volume_norm = np.linalg.norm(indata) * 10
+    if volume_norm > 700:
+        print("boo")
+        client.publish('LoundNoise', 'Alarm', qos=0, retain=False)
+        currAlarm = 2
+        # print(f'[{(datetime.datetime.today()).strftime("%H:%M:%S")}] Published')
+
 
 broker_address = "192.168.0.123"
 client = mqtt.Client()
@@ -83,7 +86,7 @@ client.subscribe([("alarm", 0), ("alarmclock", 0)])
 stream = sd.InputStream(callback=audio_callback)
 while True:
     time.sleep(1)
-    print(datetime.now().hour, datetime.now().minute,int(alarmtim[0]),int(alarmtim[1]))
+    print(datetime.now().hour, datetime.now().minute, int(alarmtim[0]), int(alarmtim[1]))
     if currAlarm == 1:
         warningAlarm()
         currAlarm = 0
