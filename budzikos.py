e28b3880e0b1a5613c181975e2fb4a262872b194
maciekbaczmanski <cakeisalie16@gmail.com>
2022-01-16 09:41:28 +0100

    removed prints

--- budzikos.py
+++ budzikos.py
@@ -1,7 +1,12 @@
 import RPi.GPIO as GPIO
-import paho.mqtt.client as mqtt
 import time
 from datetime import datetime, date
+import numpy as np
+import sounddevice as sd
+import paho.mqtt.client as mqtt
+import datetime
+
+
 
 alarmen = False
 alarmtim = ('0', '0')
@@ -30,17 +35,25 @@ def on_message(client, userdata, message):
 
 def startAlarm():
     GPIO.output(alarmPin, GPIO.HIGH)
-    time.sleep(0.2)
+    time.sleep(0.1)
     GPIO.output(alarmPin, GPIO.LOW)
-    time.sleep(0.2)
+    time.sleep(0.1)
 
 
 def warningAlarm():
-    for i in range(10):
+    for i in range(5):
         GPIO.output(alarmPin, GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(alarmPin, GPIO.LOW)
         time.sleep(0.5)
+    GPIO.output(alarmPin, GPIO.HIGH)
+
+def bigAlarm():
+    for i in range(5):
+        GPIO.output(alarmPin, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(alarmPin, GPIO.LOW)
+        time.sleep(0.2)
     GPIO.output(alarmPin, GPIO.HIGH)
 
 
@@ -52,6 +65,14 @@ def clock():
     global h, min, clockAlarm
 
 
+def audio_callback(indata, frames, time, status):
+   global client, currAlarm
+   volume_norm = np.linalg.norm(indata) * 10
+   if volume_norm > 700:
+      client.publish(f'[{(datetime.datetime.today()).strftime("%H:%M:%S")}] LoundNoise', 'Alarm', qos=0, retain=False)
+      currAlarm = 2
+      # print(f'[{(datetime.datetime.today()).strftime("%H:%M:%S")}] Published')
+
 broker_address = "192.168.0.123"
 client = mqtt.Client()
 client.username_pw_set("Raspberry_Pi", "Rpi_Raspberry_Python")
@@ -59,12 +80,15 @@ client.on_message = on_message
 client.connect(broker_address, 1881)
 client.loop_start()
 client.subscribe([("alarm", 0), ("alarmclock", 0)])
-
+stream = sd.InputStream(callback=audio_callback)
 while True:
     time.sleep(1)
     print(datetime.now().hour, datetime.now().minute,int(alarmtim[0]),int(alarmtim[1]))
-    if currAlarm:
+    if currAlarm == 1:
         warningAlarm()
+        currAlarm = 0
+    elif currAlarm == 2:
+        bigAlarm()
         currAlarm = 0
     if datetime.now().hour == int(alarmtim[0]) and datetime.now().minute == int(alarmtim[1]):
         while clockAlarm:
