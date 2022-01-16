e727339cc91a277d044d4f3ea8b5e66543699845
maciekbaczmanski <cakeisalie16@gmail.com>
2022-01-16 07:40:05 +0100

    removed prints

--- dupik.py
+++ dupik.py
@@ -7,14 +7,18 @@ direction = 0
 pir = MotionSensor(4)
 servo = Servo(17)
 servo.detach()
+piren = False
 
 
 def on_message(client, userdata, message):
-    global direction
+    global direction, piren
     if message.topic == "position":
         # client.publish("test2", "ok", qos=0, retain=False)
         direction = int(message.payload.decode("utf-8"))
-
+    elif message.topic == "alarm" and str(message.payload.decode("utf-8")) == "pir1":
+        piren = True
+    elif message.topic == "alarm" and str(message.payload.decode("utf-8")) == "pir0":
+        piren = False
 
 
 broker_address = "192.168.0.123"
@@ -30,21 +34,24 @@ client.subscribe([("position", 0), ])
 while True:
     motion = 0
     if direction == -1:
+        piren = False
         servo.max()
         time.sleep(0.02)
         servo.detach()
         direction = 0
     elif direction == 1:
+        piern = False
         servo.min()
         time.sleep(0.02)
         servo.detach()
         direction = 0
-    for i in range(30):
-        if pir.motion_detected:
-            motion += 1
-        time.sleep(0.01)
-        if motion > 10:
-            print('alarm')
-            client.publish("alarm", "pir", qos=0, retain=False)
-            time.sleep(2)
-            break
+    if piren:
+        for i in range(30):
+            if pir.motion_detected:
+                motion += 1
+            time.sleep(0.01)
+            if motion > 10:
+                print('alarm')
+                client.publish("alarm", "pir", qos=0, retain=False)
+                time.sleep(2)
+                break
