84941072a83f9a08501a1c660ea75dd62188c6c8
pudliss <jakub.pudlo@outlook.com>
2022-01-16 07:06:48 +0100

    Adding mqtt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cgitb import reset
 import RPi.GPIO as GPIO
 import time
 from telegram import Telegram
+import paho.mqtt.client as mqtt
 
 global knock
 global doors
@@ -11,6 +12,8 @@ knock = 0
 rreset_door = 0
 rreset_knock = 0
 
+# alarm
+
 #GPIO SETUP
 channel = 17
 channel_pir = 27
@@ -18,33 +21,33 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 GPIO.setup(channel_pir, GPIO.IN) 
 def callback(channel):
-        if GPIO.input(channel):
-                print ('Sound Detected!')
-                global knock
-                knock += 1
-
-        else:
-                print ('Sound Detected!')
-                global knock
-                knock += 1
+    global knock
+    print ('Sound Detected!')
+    knock += 1
 
 def callback_pir(channel_pir):
-        if GPIO.input(channel_pir):
-                print ('switch')
-                global doors
-                doors += 1
-        else:
-                print ('switch')
-                global doors
-                doors += 1
+    global doors
+    doors += 1
 
 
 bocik = Telegram()
+
+def on_message(client, userdata, message):
+    global bocik
+    bocik.msg_all('Alarm detected lmao')
+    print("message received")
+
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=200)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 GPIO.add_event_detect(channel_pir, GPIO.BOTH, bouncetime=1000)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel_pir, callback_pir)  # assign function to GPIO PIN, Run function on change
+
+client = mqtt.Client()
+client.on_message=on_message 
+client.connect("192.168.0.123", 1881) 
+client.loop_start() 
+client.subscribe("alarm")
 
 while True:
     if doors > 5 & door_open == 0:
@@ -52,7 +55,7 @@ while True:
         print('The door was opened')
         bocik.msg_all('The door was opened')
 
-    if knock > 1:
+    if knock > 3:
         print('knock knock')
         bocik.msg_all('knock knock')
     if rreset_door == doors:
